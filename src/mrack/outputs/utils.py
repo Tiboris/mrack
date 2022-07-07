b2c456a2b39c0b150a61ee0422d763179a810263
Tibor Dudlák <tdudlak@redhat.com>
2022-07-07 19:13:54 +0200

    feat: Wait for the DNS record for up to 5 minutes
    Signed-off-by: Tibor Dudlák <tdudlak@redhat.com>

--- src/mrack/outputs/utils.py
+++ src/mrack/outputs/utils.py
@@ -14,18 +14,44 @@
 
 """Utility functions for output modules."""
 
+import logging
 import socket
+from datetime import datetime, timedelta
 from socket import error as socket_error
+from time import sleep
 
 from mrack.utils import find_value_in_config_hierarchy
 
+logger = logging.getLogger(__name__)
 
-def resolve_hostname(ip_addr):
-    """Resolve IP address to hostname."""
-    try:
-        return socket.gethostbyaddr(ip_addr)[0]
-    except socket_error:
-        return None
+
+def resolve_hostname(ip_addr, timeout_minutes=2, sleep_seconds=10):
+    """
+    Resolve IP address to hostname.
+
+    Try to resolve IP address to the hostname in the specified timeout.
+    """
+    timeout_time = datetime.now() + timedelta(minutes=timeout_minutes)
+    res = None
+    logger.info(
+        f"Waiting up to {timeout_minutes} minute(s) for host "
+        f" with ip {ip_addr} to have DNS entry."
+    )
+
+    # In some cases it takes time to create DNS record
+    while datetime.now() < timeout_time:
+        try:
+            res = socket.gethostbyaddr(ip_addr)[0]
+            if res:
+                break
+
+            sleep(sleep_seconds)
+
+        except socket_error:
+            pass
+
+    logger.debug(f"Result of the DNS lookup: {res if res else 'Failed'}")
+    return res
 
 
 def get_external_id(host, meta_host, config):
